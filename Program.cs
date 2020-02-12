039cb1e5bd0a9d87385cb03855b0234608270286
Rathi <rathiram7583@gmail.com>
2020-02-12 11:26:32 -0700

    Added Web Files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+
+namespace C_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=23;
+            Console.WriteLine("Hello World!"+x);
+            if (x<30)
+            {
+                Console.WriteLine("X is less than thirty");
+            }else if(x==30)
+            {Console.WriteLine("X is Thirty");
+            }
+            else{
+                Console.WriteLine("x is greater than Thirty");
+            }
+            
+            
+            
+            
+            
+
+        }
+    }
+}
